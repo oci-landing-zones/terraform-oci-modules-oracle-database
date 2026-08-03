9dd8a84b363eeddd3927e9052280a9e736ce4893
Andre Correa <andre.correa@oracle.com>
2026-08-03 18:45:17 -0300

    feat: recovery service module

---
+++ autonomous-recovery-service/examples/vision/variables.tf
@@ -0,0 +1,15 @@
+# Copyright (c) 2026 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+variable "tenancy_ocid" {}
+variable "region" { description = "Your tenancy region" }
+variable "user_ocid" { default = "" }
+variable "fingerprint" { default = "" }
+variable "private_key_path" { default = "" }
+variable "private_key_password" { default = "" }
+
+variable "rcv_configuration" {
+  description = "Recovery Service configuration attributes."
+  type        = any
+  default     = null
+}
